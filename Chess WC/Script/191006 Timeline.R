55d465a6ad033bf32931b75b68c4eb5307465078
flr.adam <flr.adam@gmail.com>
2019-10-17 20:46:48 +0200

    Improved timeline

--- Chess WC/Script/191006 Timeline.R
+++ Chess WC/Script/191006 Timeline.R
@@ -4,11 +4,16 @@ library(ggplot2)
 library(reshape2)
 library(magrittr)
 library(dplyr)
+library(cowplot)
+
+dat_matches$Champion2 <- unlist(lapply(strsplit(dat_matches$Champion2, " "), function(x) x[length(x)]))
+dat_matches$`Runner-up` <- unlist(lapply(strsplit(dat_matches$`Runner-up`, " "), function(x) x[length(x)]))
 
 dat_matches$Champion2NA <- dat_matches$Champion2
 for(i in 2:dim(dat_matches)[1]){
   if (dat_matches$Champion2[i] == dat_matches$Champion2[i-1]) {dat_matches$Champion2NA[i] <- NA}
 }
+
 
 # Timeline with names
 dat_matches %>% 
@@ -26,12 +31,12 @@ g <- dat_matches %>%
   melt(c("Champion2", "cycle", "Status")) %>% 
   mutate(Champion2 = factor(Champion2, levels = rev(unique(Champion2)), ordered = T)) %>% 
   ggplot(aes(value, Champion2, group = cycle, col = Status)) + 
-    geom_line(size = 1) +
-    xlab("") + ylab("") +
-    theme_bw() +
-    scale_color_discrete(breaks = c("Unofficial", "Official WCC",
-                                    "Official FIDE WCC", "Classical WCC", "WCC")) +
-    theme(legend.title = element_blank())
+  geom_line(size = 1) +
+  xlab("") + ylab("") +
+  theme_bw() +
+  scale_color_discrete(breaks = c("Unofficial", "Official WCC",
+                                  "Official FIDE WCC", "Classical WCC", "WCC")) +
+  theme(legend.title = element_blank())
 g
 #ggsave("Chess WC/Output/Timeline.pdf", g)
 
@@ -39,11 +44,18 @@ g
 dat_matches %>% 
   filter(Status != "FIDE WCC", Year > 1900) %>% 
   mutate(ChampionNA = ifelse(c(T, Champion2[-1] != Champion2[-length(Champion2)]),
-                             Champion2, NA)) %>% 
-  ggplot(aes(Year, 0)) + 
-  geom_point(aes(col = is.na(ChampionNA)), size = 5) +
-  geom_text(aes(label = ChampionNA), angle = 90, hjust = 0, vjust = 0.3, nudge_y = 0.05) +
+                             Champion2, NA),
+         ChampionOrder = cumsum(!is.na(ChampionNA)),
+         ChampionHeight = 0.5 - 0.4 * ChampionOrder/max(ChampionOrder)) -> dat_temp
+ggplot(dat_temp, aes(Year, 0)) + 
+  geom_text(aes(label = ChampionNA, y = ChampionHeight), angle = 0, hjust = 0, vjust = 0, nudge_y = 0.01) +
   geom_text(aes(label = `Runner-up`), angle = 90, hjust = 1, vjust = 0.3, nudge_y = -0.05) +
-  ylim(-1, 1) +
+  geom_line(aes(x, y, group = x), data = data.frame(x = rep(dat_temp$Year[!is.na(dat_temp$ChampionNA)], 2),
+                                                    y = c(dat_temp$ChampionHeight[!is.na(dat_temp$ChampionNA)], rep(0, 18)))) +
+  geom_line(aes(x, 0.5, group = gr), data = data.frame(x = c(dat_temp$Year[!is.na(dat_temp$ChampionNA)], dat_temp$Year[!is.na(dat_temp$ChampionNA)][-1] - 1, 2019), gr = 1:18)) +
+  geom_point(aes(col = is.na(ChampionNA)), size = 5) +
+  geom_text(aes(label = Year), size = 1.5, col = ifelse(is.na(dat_temp$ChampionNA), "white", "black")) + 
+  ylim(-0.5, 0.6) +
   theme(legend.position = "none") +
-  scale_color_manual(values = c("red", "black"))
+  scale_color_manual(values = c("red", "black")) +
+  theme_nothing()
