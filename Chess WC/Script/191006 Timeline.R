c7d108606d23ef5edf01cd7647ab619b578fafe0
flr.adam <flr.adam@gmail.com>
2019-10-29 19:50:11 +0100

    ELO timeline plot

--- Chess WC/Script/191006 Timeline.R
+++ Chess WC/Script/191006 Timeline.R
@@ -41,14 +41,15 @@ g <- dat_matches %>%
 g
 #ggsave("Chess WC/Output/Timeline.pdf", g)
 
-## Champions, timeline
+# Champions, timeline ----
 dat_matches %>% 
   filter(Status != "FIDE WCC", Year > 1920) %>% 
   mutate(ChampionNA = ifelse(c(T, Champion2[-1] != Champion2[-length(Champion2)]),
                              Champion2, NA),
          ChampionOrder = cumsum(!is.na(ChampionNA)),
-         ChampionHeight = 0.5 - 0.4 * ChampionOrder/max(ChampionOrder),
+         ChampionHeight = 0.5 - 0.4 * ChampionOrder/max(ChampionOrder) + 1,
          `Runner-up` = ifelse(`Runner-up` == "players", "Tournament", `Runner-up`)) -> dat_temp
+
 g <- ggplot(dat_temp, aes(Year, 0)) + 
   geom_text(aes(label = ChampionNA, y = ChampionHeight), angle = 0, hjust = 0, vjust = 0, nudge_y = 0.01) +
   geom_text(aes(label = `Runner-up`, x = Year), 
@@ -67,4 +68,43 @@ g <- ggplot(dat_temp, aes(Year, 0)) +
   scale_color_manual(values = c("red", "black")) +
   theme_nothing()
 g
-ggsave("Chess WC/Output/Timeline2.pdf", g, width = 25, height = 10)
+#ggsave("Chess WC/Output/Timeline2.pdf", g, width = 25, height = 10)
+
+
+# Champions and ELO timeline ----
+dat_elo <- read.csv("Chess WC/Data/merged_elo_series.csv")
+dat_elo %>% 
+  filter(Year > 1920) %>% 
+  mutate(RatingStand = (Rating - min(Rating)) / (max(Rating) - min(Rating))) -> dat_elo
+
+g2 <- ggplot(dat_temp, aes(Year, 0)) + 
+  geom_text(aes(label = ChampionNA, y = ChampionHeight), angle = 0, hjust = 0, vjust = 0, nudge_y = 0.01) +
+  geom_text(aes(label = `Runner-up`, x = Year), 
+            angle = 90, hjust = 1, vjust = 0.35, nudge_y = -0.045) +
+  geom_line(aes(Year + (Month - 1) / 12, RatingStand, group = Ranking), inherit.aes = F, 
+            data = dat_elo %>% filter(Year > 1920), alpha = 0.2) +
+  geom_text(aes(x, y, label = text), 
+            data = data.frame(x = 1948, y = -0.4, text = "In 1948, Botvinnik places first \nin the Hague-Moscow tournament, \nahead of Smyslov, Keres, Reshevsky \nand Euwe."), 
+            hjust = 0, nudge_x = -0.5, size = 2) +
+  geom_text(aes(x, y, label = text), 
+            data = data.frame(x = 2007, y = -0.4, text = "In 2007, Anand places first \nin the Mexico City tournament, \nahead of Kramnik, Gelfand, \nLeko, Svidler, Morozevich, \nAronian and Grischuk."), 
+            hjust = 0, size = 2, nudge_x = -0.5) +
+  geom_smooth(aes(Year + (Month - 1) / 12, RatingStand, group = Champion), 
+              data = dat_elo %>% filter(Champion.status == "Champion", Year > 1920), 
+              inherit.aes = F, col = "red", se = F) +
+  geom_line(aes(x, y, group = x), data = data.frame(x = rep(dat_temp$Year[!is.na(dat_temp$ChampionNA)], 2),
+                                                    y = c(dat_temp$ChampionHeight[!is.na(dat_temp$ChampionNA)], rep(0, 17)))) + 
+  geom_line(aes(x, y, group = gr), 
+            data = data.frame(x = c(dat_temp$Year[!is.na(dat_temp$ChampionNA)], 
+                                    dat_temp$Year[!is.na(dat_temp$ChampionNA)][-1] - 1, 2019), 
+                              gr = 1:17,
+                              y = dat_temp$ChampionHeight[!is.na(dat_temp$ChampionNA)])) +
+  geom_point(aes(col = is.na(ChampionNA)), size = 5) +
+  geom_text(aes(label = Year), size = 1.5, col = ifelse(is.na(dat_temp$ChampionNA), "white", "black")) + 
+  ylim(-0.5, 2) +
+  theme(legend.position = "none") +
+  scale_color_manual(values = c("red", "black")) +
+  theme_nothing()
+g2
+
+ggsave("Chess WC/Output/Timeline4.pdf", g2, width = 25, height = 10)
