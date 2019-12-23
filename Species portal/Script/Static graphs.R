f8d95a28a6ac3191d6441ccd9816bc5714cbfb5e
Adam Flöhr <flr.adam@gmail.com>
2019-12-23 15:54:15 +0100

    Monthly border maps

--- Species portal/Script/Static graphs.R
+++ Species portal/Script/Static graphs.R
@@ -20,4 +20,31 @@ ggplot() +
   coord_equal()
 
 # Swedish border
-dat_swe@polygons %>% class()
+dat_swe_borders <- as.data.frame(dat_swe@polygons[[1]]@Polygons[[1]]@coords)
+dat_swe_borders$id <- 1
+dat_swe_borders$hole <- dat_swe@polygons[[1]]@Polygons[[1]]@hole
+for(i in 2:36){
+  dat <- as.data.frame(dat_swe@polygons[[1]]@Polygons[[i]]@coords)
+  dat$id <- i
+  dat$hole <- dat_swe@polygons[[1]]@Polygons[[i]]@hole
+  dat_swe_borders <- rbind(dat_swe_borders, dat)
+}
+
+## Single map w. islands and lakes
+ggplot(dat_swe_borders, aes(V1, V2, group = id, fill = hole)) + geom_polygon() + coord_equal()
+
+## Standardized
+dat_swe_borders %>% 
+  mutate(V1 = (V1 - min(V1)) / (max(V2) - min(V2)),
+         V2 = (V2 - min(V2)) / (max(V2) - min(V2))) -> dat_swe_borders
+
+l <- dim(dat_swe_borders)[1]
+dat_swe_borders[rep(1:l, 12),] %>% 
+  mutate(month = rep(1:12, each = l),
+         row = rep(c(1,1,1,2,2,2,3,3,3,4,4,4), each = l),
+         col = rep(c(1,2,3,1,2,3,1,2,3,1,2,3), each = l)) -> dat_swe_borders
+
+## Monthly map
+ggplot(dat_swe_borders, aes(V1 + col/2, V2 + row, group = paste0(month, "_", id), fill = paste0(month, "_", hole))) +
+  geom_polygon(col = "black") +
+  coord_equal()
