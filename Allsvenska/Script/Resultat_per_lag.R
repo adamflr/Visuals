1d7a9f9843c1aefdb82c2fe694fb5d7e974139eb
flr.adam <flr.adam@gmail.com>
2020-10-21 19:41:26 +0200

    Added colors for lines

--- Allsvenska/Script/Resultat_per_lag.R
+++ Allsvenska/Script/Resultat_per_lag.R
@@ -8,7 +8,8 @@ dat_long <- dat_match %>%
   mutate(segrare = ifelse(hemmamal == bortamal, "lika", 
                           ifelse(hemmamal > bortamal, "hemma", "borta")),
          id = 1:n()) %>% 
-  pivot_longer(-c(hemmamal, bortamal, datum, sasong, segrare, id), names_to = "plats", values_to = "lag") %>% 
+  pivot_longer(-c(hemmamal, bortamal, datum, sasong, segrare, id), 
+               names_to = "plats", values_to = "lag") %>% 
   mutate(status = ifelse(plats == segrare, "seger",
                          ifelse(segrare == "lika", "lika", "förlust")),
          gjorda_mål = ifelse(plats == "hemma", hemmamal, bortamal),
@@ -31,3 +32,17 @@ g1 <- dat_long %>%
   geom_line(data = . %>% filter(lag == "Malmö"), col = "lightblue", size = 3) +
   facet_wrap(~ gsub("_", "-", sasong), scale = "free_x", strip.position = "left")
 g1
+
+dat_long <- dat_long %>% 
+  left_join(dat_teams_col %>% rename("lag" = "Lag")) %>% 
+  mutate(Color = ifelse(is.na(Color), "grey20", Color)) %>% 
+  filter(sasong == 2010)
+
+dat_long <- dat_long %>% 
+  arrange(lag) %>% 
+  filter(Color != "grey20")
+g2 <- ggplot(dat_long, aes(omgång, poäng, col = lag)) +
+  geom_smooth(size = 2, span = 0.2, se = F) +
+  scale_color_manual(values = unique(dat_long$Color)) +
+  facet_wrap(~ gsub("_", "-", sasong), scale = "free_x", strip.position = "left")
+g2
