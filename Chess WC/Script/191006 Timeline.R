29dd64f14a91601675051133e35fed85f7af075c
malmogram <flradam@gmail.com>
2021-12-15 22:12:43 +0100

    Change font

--- Chess WC/Script/191006 Timeline.R
+++ Chess WC/Script/191006 Timeline.R
@@ -81,9 +81,9 @@ dat_elo %>%
 dat_temp %>% mutate(ChampionHeight = ifelse(Champion2 %in% c("Smyslov", "Tal", "Petrosian"), 1.2, 1.3)) -> dat_temp
 
 g2 <- ggplot(dat_temp, aes(Year, 0)) + 
-  geom_text(aes(label = ChampionNA, y = ChampionHeight), angle = 0, hjust = 0, vjust = 0, nudge_y = 0.01) +
+  geom_text(aes(label = ChampionNA, y = ChampionHeight), angle = 0, hjust = 0, vjust = 0, nudge_y = 0.01, family = "serif") +
   geom_text(aes(label = `Runner-up`, x = Year), 
-            angle = 90, hjust = 1, vjust = 0.35, nudge_y = -0.045) +
+            angle = 90, hjust = 1, vjust = 0.35, nudge_y = -0.045, family = "serif") +
   geom_line(aes(Year + (Month - 1) / 12, RatingStand, group = Ranking), inherit.aes = F, 
             data = dat_elo %>% filter(Year > 1920), alpha = 0.2) +
   geom_text(aes(x, y, label = text), 
@@ -91,7 +91,7 @@ g2 <- ggplot(dat_temp, aes(Year, 0)) +
                               text = paste0("In 1948, Botvinnik places first \n",
                                             "in the Hague-Moscow tournament, \n",
                                             "ahead of Smyslov, Keres, Reshevsky \nand Euwe.")), 
-            hjust = 0, nudge_x = -0.5, size = 2) +
+            hjust = 0, nudge_x = -0.5, size = 2, family = "serif") +
   geom_text(aes(x, y, label = text), 
             data = data.frame(x = 2007, y = -0.5, 
                               text = paste0("In 2007, Anand places first \n",
@@ -99,14 +99,14 @@ g2 <- ggplot(dat_temp, aes(Year, 0)) +
                                             "ahead of Kramnik, Gelfand, \n",
                                             "Leko, Svidler, Morozevich, \n",
                                             "Aronian and Grischuk.")), 
-            hjust = 0, size = 2, nudge_x = -0.5) +
+            hjust = 0, size = 2, nudge_x = -0.5, family = "serif") +
   geom_text(aes(x, y, label = text), 
             data = data.frame(x = 1975, y = -0.5, 
-                              text = paste0("In 1974, Karpov wins the 1974 Candidates\n",
+                              text = paste0("In 1974, Karpov wins the Candidates\n",
                                             "in a final versus Korchnoi,\n",
                                             "and becomes world champion in 1975 as Fisher\n", 
                                             "forfeits the championship match.")), 
-            hjust = 0, size = 2, nudge_x = -0.5) +
+            hjust = 0, size = 2, nudge_x = -0.5, family = "serif") +
   geom_smooth(aes(Year + (Month - 1) / 12, RatingStand, group = Champion), 
               data = dat_elo %>% filter(Champion.status == "Champion", Year > 1920), 
               inherit.aes = F, col = "red", se = F) +
@@ -118,7 +118,7 @@ g2 <- ggplot(dat_temp, aes(Year, 0)) +
                               gr = 1:17,
                               y = dat_temp$ChampionHeight[!is.na(dat_temp$ChampionNA)])) +
   geom_point(aes(col = is.na(ChampionNA)), size = 5) +
-  geom_text(aes(label = Year), size = 1.5, col = ifelse(is.na(dat_temp$ChampionNA), "white", "black")) + 
+  geom_text(aes(label = Year), size = 1.5, col = ifelse(is.na(dat_temp$ChampionNA), "white", "black"), family = "serif") + 
   ylim(-0.95, 1.7) +
   theme(legend.position = "none") +
   scale_color_manual(values = c("red", "black")) +
@@ -128,8 +128,9 @@ g2 <- ggplot(dat_temp, aes(Year, 0)) +
                           "Follows the Kasparov continuity 1993 - 2005.\n",
                           "Ratings of world top ten given as grey lines.\n",
                           "Red line is smoothed rating curve of the world champion.\n",
-                          "Ratings are Chessmetrics (- June 2000) and FIDE (July 2000 -)."))
+                          "Ratings are Chessmetrics (- June 2000) and FIDE (July 2000 -)."),
+           family = "serif")
 g2
 
 # ggsave("Chess WC/Output/Timeline6.pdf", g2, width = 59.4, height = 42/2, units = "cm")
-ggsave("Chess WC/Output/Timeline6.png", g2, width = 65.4, height = 42/2, units = "cm")
+# ggsave("Chess WC/Output/Timeline6.png", g2, width = 65.4, height = 42/2, units = "cm")
